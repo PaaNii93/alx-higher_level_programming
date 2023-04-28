b4e315ce3b22647956f9b942c565abd8905427bd
PaaNii93 <nuertere1993@yahoo.com>
2023-04-28 22:59:17 +0000

    another

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-# fetches website
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
 import urllib.request
 
+
+def main():
+    """
+    Funtion to print a response of a specific url
+    """
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        content = response.read()
-        url = content.decode('utf-8')
-        typed = response.info()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(url))
+    main()
