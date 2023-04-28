1f91b2b56a64f4ba0d64dff5bacd5ad183d0a7cd
PaaNii93 <nuertere1993@yahoo.com>
2023-04-28 23:38:04 +0000

    try

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the comtent-lenght of a giving ip address
-curl -sI "$1" | awk '/Content-Length/{print $2}'
+#size of content-length
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
