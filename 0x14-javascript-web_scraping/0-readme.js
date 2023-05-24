e764f90b226bb2c9154761ee94f224208b7167a1
PaaNii93 <nuertere1993@yahoo.com>
2023-05-24 14:37:54 +0000

    another try

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,7 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-/* Function that reads a file */
-function fileRead (filePath) {
-  fs.readFile(filePath, 'utf-8', (error, content) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(content);
-    }
-  });
-}
-
-const args = process.argv.slice(2);
-
-if (args.length < 1) {
-  console.log('No args passed!');
-} else {
-  const filePath = args[0];
-  fileRead(filePath);
-}
+let file = process.argv[2];
+let fs = require('fs');
+fs.readFile(file, 'utf8', function (err, data = err) {
+  console.log(data);
+});
