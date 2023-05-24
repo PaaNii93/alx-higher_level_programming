053e6c6cdf2569baba83cb008e5e4179f7c91938
PaaNii93 <nuertere1993@yahoo.com>
2023-05-24 14:36:21 +0000

    try

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,22 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+
+/* Function that reads a file */
+function fileRead (filePath) {
+  fs.readFile(filePath, 'utf-8', (error, content) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(content);
+    }
+  });
+}
+
+const args = process.argv.slice(2);
+
+if (args.length < 1) {
+  console.log('No args passed!');
+} else {
+  const filePath = args[0];
+  fileRead(filePath);
+}
