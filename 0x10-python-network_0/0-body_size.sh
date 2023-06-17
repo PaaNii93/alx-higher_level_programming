639663d0d7114228a3c2899e9f4b1dd42fc7d11a
PaaNii93 <nuertere1993@yahoo.com>
2023-06-17 14:07:07 +0000

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-#size of content-length
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+
+# Check if the URL argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: ./script.sh <URL>"
+    exit 1
+fi
+
+# Extract the URL argument
+url="$1"
+
+# Send a request to the URL and store the response in a temporary file
+response_file=$(mktemp)
+curl -s -o "$response_file" -w "%{size_download}" "$url" > /dev/null
+
+# Display the size of the body in bytes
+size=$(cat "$response_file")
+echo "Size of the body: $size bytes"
+
+# Clean up the temporary file
+rm "$response_file"
+
